70185ec5285860d5c34e656f60bfaddbf1a02b30
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-06-16 10:27:28 -0600

    updat chat04

--- src/components/Template/Chart04.tsx
+++ src/components/Template/Chart04.tsx
@@ -28,132 +28,6 @@ export const Chart04 =  component$(({ IDdivContainer, autosize = true }:Props) =
       return null
     })
 
-    const chartOptions = $((size : number)=>{ 
-      return {
-      series: [
-      {
-        name: 'Bob',
-        data: [
-          {
-            x: 'Design',
-            y: [
-              new Date('2019-03-05').getTime(),
-              new Date('2019-03-08').getTime()
-            ]
-          },
-          {
-            x: 'Code',
-            y: [
-              new Date('2019-03-08').getTime(),
-              new Date('2019-03-11').getTime()
-            ]
-          },
-          {
-            x: 'Test',
-            y: [
-              new Date('2019-03-11').getTime(),
-              new Date('2019-03-16').getTime()
-            ]
-          }
-        ]
-      },
-      {
-        name: 'Joe',
-        data: [
-          {
-            x: 'Design',
-            y: [
-              new Date('2019-03-02').getTime(),
-              new Date('2019-03-05').getTime()
-            ]
-          },
-          {
-            x: 'Code',
-            y: [
-              new Date('2019-03-06').getTime(),
-              new Date('2019-03-09').getTime()
-            ]
-          },
-          {
-            x: 'Test',
-            y: [
-              new Date('2019-03-10').getTime(),
-              new Date('2019-03-19').getTime()
-            ]
-          }
-        ]
-      }
-    ],
-    chart: {
-      width: size,
-      type: 'rangeBar'
-    },
-    plotOptions: {
-      bar: {
-        horizontal: true
-      }
-    },
-    dataLabels: {
-      enabled: true,
-      formatter: function(val:any) {
-        const a = moment(val[0])
-        const b = moment(val[1])
-        const diff = b.diff(a, 'days')
-        return diff + (diff > 1 ? ' days' : ' day')
-      }
-    },
-    fill: {
-      type: 'gradient',
-      gradient: {
-        shade: 'light',
-        type: 'vertical',
-        shadeIntensity: 0.25,
-        gradientToColors: undefined,
-        inverseColors: true,
-        opacityFrom: 1,
-        opacityTo: 1,
-        stops: [50, 0, 100, 100]
-      }
-    },
-    xaxis: {
-      type: 'datetime'
-    },
-    legend: {
-      position: 'top'
-    },
-    responsive: [
-      {
-        breakpoint: 1024,
-        options: {
-          chart: {
-            height: 300,
-          },
-        },
-      },
-      {
-        breakpoint: 1366,
-        options: {
-          chart: {
-            height: 350,
-          },
-        },
-      },
-    ],
-    
-    grid: {
-      xaxis: {
-        lines: {
-          show: true,
-        },
-      },
-      yaxis: {
-        lines: {
-          show: true,
-        },
-      },
-    },
-      }
-    })
 
     
 
@@ -165,7 +39,154 @@ export const Chart04 =  component$(({ IDdivContainer, autosize = true }:Props) =
       })
 
       //-- Option Chart --
-      const chartfourOptions = await chartOptions(widthDivContainer.value)
+      const chartfourOptions = {
+        series: [
+        {
+          name: 'Bob',
+          data: [
+            {
+              x: 'Design',
+              y: [
+                new Date('2019-03-05').getTime(),
+                new Date('2019-03-08').getTime()
+              ]
+            },
+            {
+              x: 'Code',
+              y: [
+                new Date('2019-03-08').getTime(),
+                new Date('2019-03-11').getTime()
+              ]
+            },
+            {
+              x: 'Test',
+              y: [
+                new Date('2019-03-11').getTime(),
+                new Date('2019-03-16').getTime()
+              ]
+            }
+          ]
+        },
+        {
+          name: 'Joe',
+          data: [
+            {
+              x: 'Design',
+              y: [
+                new Date('2019-03-02').getTime(),
+                new Date('2019-03-05').getTime()
+              ]
+            },
+            {
+              x: 'Code',
+              y: [
+                new Date('2019-03-06').getTime(),
+                new Date('2019-03-09').getTime()
+              ]
+            },
+            {
+              x: 'Test',
+              y: [
+                new Date('2019-03-10').getTime(),
+                new Date('2019-03-19').getTime()
+              ]
+            }
+          ]
+        }
+      ],
+      chart: {
+        width: widthDivContainer.value,
+        type: 'rangeBar'
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function(val:any) {
+          const a = moment(val[0])
+          const b = moment(val[1])
+          const diff = b.diff(a, 'days')
+          return diff + (diff > 1 ? ' days' : ' day')
+        }
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          type: 'vertical',
+          shadeIntensity: 0.25,
+          gradientToColors: undefined,
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [50, 0, 100, 100]
+        }
+      },
+      xaxis: {
+        type: 'datetime'
+      },
+      legend: {
+        position: 'top'
+      },
+      responsive: [
+        {
+          breakpoint: 400,
+          options: {
+            chart: {
+              width: 390
+            },
+          },
+        },
+        {
+          breakpoint: 700,
+          options: {
+            chart: {
+              width: 600
+            },
+          },
+        },
+        {
+          breakpoint: 850,
+          options: {
+            chart: {
+              width: 750
+            },
+          },
+        },
+        {
+          breakpoint: 1024,
+          options: {
+            chart: {
+              width: 950
+            },
+          },
+        },
+        {
+          breakpoint: 1366,
+          options: {
+            chart: {
+              height: 300,
+            },
+          },
+        },
+      ],
+      
+      grid: {
+        xaxis: {
+          lines: {
+            show: true,
+          },
+        },
+        yaxis: {
+          lines: {
+            show: true,
+          },
+        },
+      },
+      }
 
       //-- Select div chart
       const chartSelector = document.querySelectorAll("#chartfour");
