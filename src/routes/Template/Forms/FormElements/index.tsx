99d4064c7e4e84dc4407368acd3d9df568d2bd9a
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-06-16 16:37:20 -0600

    checbox

--- src/routes/Template/Forms/FormElements/index.tsx
+++ src/routes/Template/Forms/FormElements/index.tsx
@@ -11,8 +11,6 @@ export default component$(() => {
     const checkbox04 = useSignal(false)
     const checkbox05 = useSignal(false)
     const checkbox06 = useSignal(false)
-    const checkbox07 = useSignal(false)
-    const checkbox08 = useSignal(false)
 
     return(
         <>
@@ -390,7 +388,57 @@ export default component$(() => {
                                 Checkbox Text
                                 </label>
                             </div>
-                            
+                            {/* Checkbox05 */}
+                            <div>
+                                <label for="checkboxLabelThree" class="flex cursor-pointer select-none items-center">
+                                <div class="relative">
+                                    <input type="checkbox" id="checkboxLabelThree" class="sr-only"
+                                    onClick$={()=>{
+                                        checkbox05.value = !checkbox05.value
+                                    }}
+                                    />
+                                    <div 
+                                    class={`box mr-4 flex h-5 w-5 items-center justify-center rounded border ${
+                                        checkbox05.value ? 'border-primary bg-gray' : ''
+                                    }`}>
+                                    <span  class={`text-primary opacity-0 ${
+                                        checkbox05.value ? '!opacity-100' : ''
+                                    }`}>
+                                        <svg class="h-3.5 w-3.5 stroke-current" fill="none" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M6 18L18 6M6 6l12 12"></path>
+                                        </svg>
+                                    </span>
+                                    </div>
+                                </div>
+                                Checkbox Text
+                                </label>
+                            </div>
+                            {/* Checkbox06 */}
+                            <div x-data="{ checkboxToggle: false }">
+                                <label for="checkboxLabelFour" class="flex cursor-pointer select-none items-center">
+                                <div class="relative">
+                                    <input type="checkbox" id="checkboxLabelFour" class="sr-only"
+                                    onClick$={()=>{
+                                        checkbox06.value = !checkbox06.value
+                                    }}
+                                    />
+                                    <div
+                                    class={`mr-4 flex h-5 w-5 items-center justify-center rounded-full border ${
+                                        checkbox06.value ? 'border-primary' : ''
+                                    }`}>
+                                    <span 
+                                        class={`h-2.5 w-2.5 rounded-full bg-transparent ${
+                                            checkbox06.value ? '!bg-primary' : ''
+                                        }`}>
+                                    </span>
+                                    </div>
+                                </div>
+                                Checkbox Text
+                                </label>
+                            </div>
+
                             </div> 
                         </div>
 
